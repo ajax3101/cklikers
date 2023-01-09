25d408b788304312ffed0e3330438700cb4143b3
Denys Morozov <ajax3101@gmail.com>
2023-01-09 22:21:33 +0200

    fin

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import KeyCode, Listener
 
-toggle_key = KeyCode(char='s')
+toggle_key = KeyCode(char='s') # Stop clikers
 cliking = False
 mouse = Controller()
+
 
 def kliker():
     while True:
         if cliking:
-            mouse.click(Button.left,1)
+            mouse.click(Button.left, 1)
             time.sleep(0.1)
-            
+
 
 def toggle_event(key):
     if key == toggle_key:
         global cliking
         cliking = not cliking
 
+
 def main():
-    pass
+    cliking_thread = threading.Thread(target=kliker)
+    cliking_thread.start()
+
+    with Listener(on_press=toggle_event) as listener:
+        listener.join()
+
 
 if __name__ == '__main__':
     main()
